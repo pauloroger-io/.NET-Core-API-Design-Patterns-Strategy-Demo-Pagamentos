f90363a1039797a892858e3f467740ac63584d5a
paulorgn <paulorgndev@gmail.com>
2019-10-06 15:17:55 -0300

    Controller feito

---
+++ Strategy-Pagamentos/Strategy-Pagamentos/Controllers/PagamentoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Strategy_Pagamentos.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PagamentoController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public string PrecoPorFormaDePagamento(int id)
+        {
+            return "value";
+        }
+    }
+}
